d8687e66fd8890e85c77b3885fcb7fdc65877011
Andrew <5436387+fidwell@users.noreply.github.com>
2023-10-09 20:09:09 -0400

    Finish conversion of Rock 'n' Roll Revival

--- src/scenarios/tt/rocknrollrevival.ts
+++ src/scenarios/tt/rocknrollrevival.ts
@@ -9,6 +9,18 @@ export default class TtRockNRollRevival {
     () => TtRockNRollRevival.ClearScenery(),
     // Pay off loan
     () => context.executeAction("parksetloan", <ParkSetLoanArgs>{ value: 0 }),
+    // Build 9 really tall roto-drops in test mode
+    ...TtRockNRollRevival.BuildRotoDrop(61, 41, 28),
+    ...TtRockNRollRevival.BuildRotoDrop(64, 37, 28),
+    ...TtRockNRollRevival.BuildRotoDrop(69, 38, 28),
+    ...TtRockNRollRevival.BuildRotoDrop(78, 44, 32),
+    ...TtRockNRollRevival.BuildRotoDrop(74, 46, 32),
+    ...TtRockNRollRevival.BuildRotoDrop(70, 48, 32),
+    ...TtRockNRollRevival.BuildRotoDrop(82, 49, 32),
+    ...TtRockNRollRevival.BuildRotoDrop(76, 52, 32),
+    ...TtRockNRollRevival.BuildRotoDrop(72, 54, 32),
+    // GO FAST
+    () => context.executeAction("gamesetspeed", <GameSetSpeedArgs>{ speed: 8 })
   ];
 
   static ClearScenery() {
@@ -96,73 +108,44 @@ export default class TtRockNRollRevival {
     });
   }
 
-  static Run() {
-    const start: Date = new Date();
-    let isDone: boolean = false;
-
-    let currentAction: number = 0;
+  static BuildRotoDrop(x: number, y: number, z: number): ((data: void) => void)[] {
+    const actions = [];
+    let baseHeight: number = z;
     let currentRotoDropId: number = 0;
 
-    // Zoom out once so we can see better
-    ui.mainViewport.zoom = 1;
+    actions.push(() => context.executeAction("ridecreate", <RideCreateArgs>{
+      rideType: 69, // Roto-drop track
+      rideObject: 46, // Roto-drop vehicle
+      entranceObject: 0, // ?
+      colour1: 0,
+      colour2: 0
+    }, (result: RideCreateActionResult) => {
+      currentRotoDropId = result.ride;
+    }));
+    
+    actions.push(() => context.executeAction("trackplace", <TrackPlaceArgs>{
+      x: x * 32,
+      y: y * 32,
+      z: baseHeight * 8,
+      direction: 0,
+      ride: currentRotoDropId,
+      trackType: 66, // tower base
+      rideType: 69,
+      brakeSpeed: 0,
+      colour: 0,
+      seatRotation: 0,
+      trackPlaceFlags: 0,
+      isFromTrackDesign: false
+    }));
 
-    const actions: ((data: void) => void)[] = [
-      // Build 9 really tall roto-drops in test mode
-      ...buildRotoDrop(61, 41, 28),
-      ...buildRotoDrop(64, 37, 28),
-      ...buildRotoDrop(69, 38, 28),
-      ...buildRotoDrop(78, 44, 32),
-      ...buildRotoDrop(74, 46, 32),
-      ...buildRotoDrop(70, 48, 32),
-      ...buildRotoDrop(82, 49, 32),
-      ...buildRotoDrop(76, 52, 32),
-      ...buildRotoDrop(72, 54, 32),
-      // GO FAST
-      () => context.executeAction("gamesetspeed", <GameSetSpeedArgs>{ speed: 8 })
-    ];
-
-    context.subscribe("interval.tick", () => {
-      if (!isDone && scenario.status === "completed") {
-        const finish = new Date();
-        const ms = finish.getTime() - start.getTime();
-        park.postMessage(<ParkMessageDesc>{
-          type: "award",
-          text: `Objective completed in ${(ms / 1000).toFixed(3)} seconds, ${date.ticksElapsed} ticks`
-        });
-        isDone = true;
-      }
-
-      if (currentAction >= actions.length)
-        return;
-
-      actions[currentAction]();
-      currentAction += 1;
-    });
-
-    function buildRotoDrop(x: number, y: number, z: number): ((data: void) => void)[] {
-      const actions = [];
-      let baseHeight: number = z;
-
-      // Look at what we're building, just for fun
-      actions.push(() => ui.mainViewport.moveTo(<CoordsXY>{ x: x * 32, y: y * 32 }));
-
-      actions.push(() => context.executeAction("ridecreate", <RideCreateArgs>{
-        rideType: 69, // Roto-drop track
-        rideObject: 46, // Roto-drop vehicle
-        entranceObject: 0, // ?
-        colour1: 0,
-        colour2: 0
-      }, (result: RideCreateActionResult) => {
-        currentRotoDropId = result.ride;
-      }));
-      ;
+    for (let i = 0; i < 35; i += 1) {
       actions.push(() => context.executeAction("trackplace", <TrackPlaceArgs>{
         x: x * 32,
         y: y * 32,
-        z: baseHeight * 8,
+        z: (baseHeight + 12 + i * 4) * 8,
         direction: 0,
         ride: currentRotoDropId,
-        trackType: 66, // tower base
+        trackType: 67, // tower vertical
         rideType: 69,
         brakeSpeed: 0,
         colour: 0,
@@ -170,48 +153,31 @@ export default class TtRockNRollRevival {
         trackPlaceFlags: 0,
         isFromTrackDesign: false
       }));
-
-      for (let i = 0; i < 35; i += 1) {
-        actions.push(() => context.executeAction("trackplace", <TrackPlaceArgs>{
-          x: x * 32,
-          y: y * 32,
-          z: (baseHeight + 12 + i * 4) * 8,
-          direction: 0,
-          ride: currentRotoDropId,
-          trackType: 67, // tower vertical
-          rideType: 69,
-          brakeSpeed: 0,
-          colour: 0,
-          seatRotation: 0,
-          trackPlaceFlags: 0,
-          isFromTrackDesign: false
-        }));
-      }
-
-      actions.push(() => context.executeAction("rideentranceexitplace", <RideEntranceExitPlaceArgs>{
-        x: (x - 2) * 32,
-        y: y * 32,
-        direction: 2,
-        ride: currentRotoDropId,
-        station: 0,
-        isExit: false
-      }));
-
-      actions.push(() => context.executeAction("rideentranceexitplace", <RideEntranceExitPlaceArgs>{
-        x: (x + 2) * 32,
-        y: y * 32,
-        direction: 0,
-        ride: currentRotoDropId,
-        station: 0,
-        isExit: true
-      }));
-
-      actions.push(() => context.executeAction("ridesetstatus", <RideSetStatusArgs>{
-        ride: currentRotoDropId,
-        status: 2 // testing
-      }));
-
-      return actions;
     }
+
+    actions.push(() => context.executeAction("rideentranceexitplace", <RideEntranceExitPlaceArgs>{
+      x: (x - 2) * 32,
+      y: y * 32,
+      direction: 2,
+      ride: currentRotoDropId,
+      station: 0,
+      isExit: false
+    }));
+
+    actions.push(() => context.executeAction("rideentranceexitplace", <RideEntranceExitPlaceArgs>{
+      x: (x + 2) * 32,
+      y: y * 32,
+      direction: 0,
+      ride: currentRotoDropId,
+      station: 0,
+      isExit: true
+    }));
+
+    actions.push(() => context.executeAction("ridesetstatus", <RideSetStatusArgs>{
+      ride: currentRotoDropId,
+      status: 2 // testing
+    }));
+
+    return actions;
   }
 }
