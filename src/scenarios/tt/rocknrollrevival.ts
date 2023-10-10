cbc83152986f0ffec61196ba42a60d6d983f79fe
Andrew <5436387+fidwell@users.noreply.github.com>
2023-10-09 20:18:06 -0400

    We didn't need to clear any scenery after all

--- src/scenarios/tt/rocknrollrevival.ts
+++ src/scenarios/tt/rocknrollrevival.ts
@@ -4,9 +4,6 @@ export default class TtRockNRollRevival {
   static Actions: ((data: void) => void)[] = [
     // Delete all rides
     ...ArrayUtilities.range(0, 14).map(i => () => context.executeAction("ridedemolish", <RideDemolishArgs>{ ride: i, modifyType: 0 })),
-    // Clear all footpath and scenery
-    // Need to do in batches; currently this throws lots of errors since we're doing too much at once
-    () => TtRockNRollRevival.ClearScenery(),
     // Pay off loan
     () => context.executeAction("parksetloan", <ParkSetLoanArgs>{ value: 0 }),
     // Build 9 really tall roto-drops in test mode
@@ -22,91 +19,6 @@ export default class TtRockNRollRevival {
     // GO FAST
     () => context.executeAction("gamesetspeed", <GameSetSpeedArgs>{ speed: 8 })
   ];
-
-  static ClearScenery() {
-    for (let x = 0; x < map.size.x; x += 1) {
-      for (let y = 0; y < map.size.y; y += 1) {
-        const tile = map.getTile(x, y);
-
-        const surfaceElement = <SurfaceElement>tile.elements.filter(e => e.type === "surface")[0];
-        if (!surfaceElement.hasOwnership)
-          continue;
-
-        TtRockNRollRevival.ClearFootpathAt(tile);
-        TtRockNRollRevival.ClearSmallSceneryAt(tile);
-        TtRockNRollRevival.ClearLargeSceneryAt(tile);
-        TtRockNRollRevival.ClearWallsAt(tile);
-      }
-    }
-  }
-
-  static ClearFootpathAt(tile: Tile) {
-    const footpathCoords = [];
-    for (let e = 0; e < tile.elements.length; e += 1) {
-      const element = tile.elements[e];
-      if (element.type === "footpath") {
-        footpathCoords.push({ x: tile.x * 32, y: tile.y * 32, z: element.baseHeight * 8 });
-      }
-    }
-
-    for (let i = 0; i < footpathCoords.length; i += 1) {
-      context.executeAction("footpathremove", <FootpathRemoveArgs>{
-        x: footpathCoords[i].x,
-        y: footpathCoords[i].y,
-        z: footpathCoords[i].z
-      });
-    }
-  }
-
-  static ClearSmallSceneryAt(tile: Tile) {
-    tile.elements.filter(t => t.type === "small_scenery").forEach((element: SmallSceneryElement) => {
-      const args = <SmallSceneryRemoveArgs>{
-        x: tile.x * 32,
-        y: tile.y * 32,
-        z: element.baseHeight * 8,
-        object: element.object,
-        quadrant: element.quadrant
-      };
-      context.queryAction("smallsceneryremove", args, (result: GameActionResult) => {
-        if (result.cost <= 0) {
-          context.executeAction("smallsceneryremove", args);
-        }
-      });
-    });
-  }
-
-  static ClearLargeSceneryAt(tile: Tile) {
-    tile.elements.filter(t => t.type === "large_scenery").forEach((element: LargeSceneryElement) => {
-      const args = <LargeSceneryRemoveArgs>{
-        x: tile.x * 32,
-        y: tile.y * 32,
-        z: element.baseHeight * 8,
-        direction: element.direction,
-        tileIndex: tile.x + tile.y * map.size.x // probably
-      };
-      context.queryAction("largesceneryremove", args, (result: GameActionResult) => {
-        if (result.cost <= 0) {
-          context.executeAction("largesceneryremove", args);
-        }
-      });
-    });
-  }
-
-  static ClearWallsAt(tile: Tile) {
-    tile.elements.filter(t => t.type === "wall").forEach((element: WallElement) => {
-      const args = <WallRemoveArgs>{
-        x: tile.x * 32,
-        y: tile.y * 32,
-        z: element.baseHeight * 8,
-        direction: element.direction
-      };
-      context.queryAction("wallremove", args, (result: GameActionResult) => {
-        if (result.cost <= 0) {
-          context.executeAction("wallremove", args);
-        }
-      });
-    });
-  }
 
   static BuildRotoDrop(x: number, y: number, z: number): ((data: void) => void)[] {
     const actions = [];
